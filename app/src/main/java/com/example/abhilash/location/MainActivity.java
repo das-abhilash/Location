f8aa5643f2d9afc837dd5ce65732d7639928567e
Abhilash <abhilashmyworld@gmail.com>
2016-02-01 14:15:34 +0530

    added offline mathematical calculation of distanc as comment

--- app/src/main/java/com/example/abhilash/location/MainActivity.java
+++ app/src/main/java/com/example/abhilash/location/MainActivity.java
@@ -346,6 +346,30 @@ public class MainActivity extends AppCompatActivity implements
         Button find = (Button) findViewById(R.id.find);
         find.setEnabled(true);
     }
+// calculate distacne between two points with mathematical formula
 
+    /* public double CalculationByDistance(LatLng StartP, LatLng EndP) {
+int Radius = 6371;// radius of earth in Km
+double lat1 = StartP.latitude;
+double lat2 = EndP.latitude;
+double lon1 = StartP.longitude;
+double lon2 = EndP.longitude;
+double dLat = Math.toRadians(lat2 ‐ lat1);
+double dLon = Math.toRadians(lon2 ‐ lon1);
+double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
++ Math.cos(Math.toRadians(lat1))
+* Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+* Math.sin(dLon / 2);
+double c = 2 * Math.asin(Math.sqrt(a));
+double valueResult = Radius * c;
+double km = valueResult / 1;
+DecimalFormat newFormat = new DecimalFormat("####");
+int kmInDec = Integer.valueOf(newFormat.format(km));
+double meter = valueResult % 1000;
+int meterInDec = Integer.valueOf(newFormat.format(meter));
+Log.i("Radius Value", "" + valueResult + " KM " + kmInDec
++ " Meter " + meterInDec);
+return Radius * c;
+} */
 
 }
